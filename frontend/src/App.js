d8af6e941226da1ea715a93d733ec97f05e229b9
2udy <22udy629@gmail.com>
2023-01-18 13:35:08 +0900

    :sparkles: feat: 로컬회원가입화면구현

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -2,11 +2,37 @@ import React from 'react';
 import { createBrowserRouter, RouterProvider } from 'react-router-dom';
 import Root from './routes/Root';
 import Header from './components/Header';
+import LogIn from './routes/logIn/LogIn';
+import SignUp from './routes/signUp/SignUp';
+import SignUpAgree from './components/signUp/SignUpAgree';
+import SignUpInfo from './components/signUp/SignUpInfo';
+import SignUpComplete from './components/signUp/SignUpComplete';
 
+const signUp1 = <SignUpAgree />;
+const signUp2 = <SignUpInfo />;
+const signUp3 = <SignUpComplete />;
 const router = createBrowserRouter([
   {
     path: '/',
     element: <Root />,
+    children: [
+      {
+        path: 'login/',
+        element: <LogIn />,
+      },
+      {
+        path: 'signup1/',
+        element: <SignUp content={signUp1} />,
+      },
+      {
+        path: 'signup2/',
+        element: <SignUp content={signUp2} />,
+      },
+      {
+        path: 'signup3/',
+        element: <SignUp content={signUp3} />,
+      },
+    ],
   },
 ]);
 
