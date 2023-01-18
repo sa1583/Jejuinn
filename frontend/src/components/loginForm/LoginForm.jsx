ea6c10f9d942f56ef25985a3ffbcd3e8f34074d2
10yutae29 <10yutae29@gmail.com>
2023-01-18 11:43:03 +0900

    :art:fix: 로그인 화면 수정

--- frontend/src/components/loginForm/LoginForm.jsx
+++ frontend/src/components/loginForm/LoginForm.jsx
@@ -2,18 +2,20 @@ import { styled } from '@mui/material/styles';
 import { TextField } from '@mui/material';
 import { Box } from '@mui/system';
 
-
 const CustomTextField = styled(TextField)({
   '& .MuiOutlinedInput-root': {
     '& fieldset': {
+      borderColor: '#535353',
+      opacity: '83%',
+    },
+    '&:hover fieldset': {
       borderColor: '#FF7600',
-      opacity: '83%'
-    }, '&:hover fieldset': {
+    },
+    '&.Mui-focused fieldset': {
       borderColor: '#FF7600',
-    },'&.Mui-focused fieldset': {
-      borderColor: 'green',
-    },}
-})
+    },
+  },
+});
 
 export default function LoginForm() {
   return (
@@ -32,18 +34,29 @@ export default function LoginForm() {
         label="아이디"
         style={{ width: '80%', marginTop: '6vh' }}
       />
-      <CustomTextField label="비밀번호" style={{ width: '80%', marginTop: '4vh' }} />
+      <CustomTextField
+        label="비밀번호"
+        style={{ width: '80%', marginTop: '4vh' }}
+      />
 
       {/* 아래 인풋은 버튼 컴포넌트로 바꿀거임 */}
       <input
         type="submit"
         value={'로그인'}
-        style={{ width: '80%', height: '6vh', marginTop: '6vh', background:'#FF7600', borderRadius:'38px', color:'white', border:'none', fontSize:'1.5vw'}}
+        style={{
+          width: '80%',
+          height: '6vh',
+          marginTop: '6vh',
+          background: '#FF7600',
+          borderRadius: '38px',
+          color: 'white',
+          border: 'none',
+          fontSize: '1.5vw',
+        }}
       />
 
-
       {/* 소셜 로그인 부분 */}
-      <h3 style={{marginTop: '4vh', color:'#FF7600'}}>소셜 로그인</h3>
+      <h3 style={{ marginTop: '4vh', color: '#FF7600' }}>소셜 로그인</h3>
       <Box sx={{ display: 'flex', gap: '1.5vw' }}>
         {socialBtn('images/naver_login.png')}
         {socialBtn('images/kakao_login.png')}
@@ -51,7 +64,14 @@ export default function LoginForm() {
       </Box>
 
       {/* 회원가입 및 유저 정보 찾기 부분 */}
-      <Box sx={{ display: 'flex', justifyContent:'center', alignItems:'center', marginTop:'4vh' }}>
+      <Box
+        sx={{
+          display: 'flex',
+          justifyContent: 'center',
+          alignItems: 'center',
+          marginTop: '4vh',
+        }}
+      >
         {findBtn('회원가입')}
         <p style={{ margin: '1rem', fontSize: '1vw' }}>|</p>
         {findBtn('아이디 찾기')}
@@ -62,14 +82,18 @@ export default function LoginForm() {
   );
 }
 
-
 const findBtn = (inp) => {
-  return <p style={{fontSize:'1vw'}}>{inp}</p>
-}
-
+  return <p style={{ fontSize: '1vw' }}>{inp}</p>;
+};
 
 const socialBtn = (src) => {
-  return <img src={src} alt='' style={{
-    height: '2rem',
-  }}></img>
-}
+  return (
+    <img
+      src={src}
+      alt=""
+      style={{
+        height: '2rem',
+      }}
+    ></img>
+  );
+};
