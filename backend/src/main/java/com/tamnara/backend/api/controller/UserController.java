193f799a078a92061493be35af76f7e40f07def3
lee-jaewook <jaewook_lee@outlook.com>
2023-01-20 15:03:07 +0900

    :sparkles: feat: 회원 정보 조회 구현

--- backend/src/main/java/com/tamnara/backend/api/controller/UserController.java
+++ backend/src/main/java/com/tamnara/backend/api/controller/UserController.java
@@ -2,7 +2,9 @@ package com.tamnara.backend.api.controller;
 
 import com.tamnara.backend.api.dto.request.LoginPostReq;
 import com.tamnara.backend.api.dto.request.SignupPostReq;
+import com.tamnara.backend.api.dto.response.GetUserInfoPostRes;
 import com.tamnara.backend.api.service.UserService;
+import com.tamnara.backend.config.jwt.JwtFilter;
 import com.tamnara.backend.config.jwt.TokenProvider;
 import com.tamnara.backend.db.entity.User;
 import com.tamnara.backend.db.repository.UserRepository;
@@ -10,17 +12,19 @@ import io.swagger.annotations.*;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpHeaders;
 import org.springframework.http.ResponseEntity;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.AuthenticationException;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import javax.servlet.http.HttpServletRequest;
 import javax.validation.Valid;
 import java.util.Optional;
 
 @RestController
-@RequestMapping("/api")
 @Api(tags = "회원 관련 기능 API")
 @RequiredArgsConstructor
 public class UserController {
@@ -35,7 +39,7 @@ public class UserController {
      * @param userRegisterPostReq {userId, password, nickname, email, emailReceiveAllow}
      * @return
      */
-    @PostMapping("/users")
+    @PostMapping("/api/users")
     @ApiOperation(value = "회원 가입", notes = "<strong>아이디, 패스워드, 닉네임, 이메일, 이메일 수신동의 여부</strong>을 통해 회원가입 한다.")
     @ApiResponses({
             @ApiResponse(code = 200, message = "회원 가입 성공"),
@@ -59,7 +63,7 @@ public class UserController {
      * @param loginPostReq {email, password}
      * @return
      */
-    @PostMapping("/users/login")
+    @PostMapping("/api/users/login")
     @ApiOperation(value = "로그인", notes = "<strong>이메일, 패스워드</strong>를 통해 로그인 한다.")
     @ApiResponses({
             @ApiResponse(code = 200, message = "로그인 성공"),
@@ -81,6 +85,29 @@ public class UserController {
         HttpHeaders httpHeaders = userService.getHttpHeaders(user);
 
         return ResponseEntity.status(200).headers(httpHeaders).build();
+    }
+
+    /**
+     * Access Token을 통한 회원정보 조회
+     * @return
+     */
+    @PostMapping("/auth/users")
+    @ApiOperation(value = "회원정보 조회", notes = "<strong>헤더의 엑세스토큰 정보</strong>를 통해 회원정보를 조회한다.")
+    @ApiResponses({
+            @ApiResponse(code = 200, message = "로그인 성공"),
+            @ApiResponse(code = 401, message = "UNAUTHORIZED(정보조회 실패)"),
+            @ApiResponse(code = 500, message = "서버 오류")
+    })
+    public ResponseEntity<?> getUserInfo(HttpServletRequest request){
+        String accessToken = request.getHeader(JwtFilter.ACCESS_HEADER);
+        Authentication authentication = tokenProvider.getAuthentication(accessToken.substring(7));
+        String uid = authentication.getName();
+        Optional<User> user = userRepository.findById(Long.parseLong(uid));
+
+        // 검색 결과가 없다면
+        if(user.isEmpty()) return ResponseEntity.status(401).build();
+
+        return ResponseEntity.status(200).body(GetUserInfoPostRes.from(user.get()));
     }
 
 
