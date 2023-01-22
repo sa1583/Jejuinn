e7cb81120c872247e26f0e3274509f61e9cbcb67
10yutae29 <10yutae29@gmail.com>
2023-01-22 20:30:59 +0900

    :art: fix:카카오 로그인 redux-thunk설정

--- frontend/src/api/user.js
+++ frontend/src/api/user.js
@@ -14,8 +14,16 @@ function loginNaver(token) {
   return api.post('/api/users/social/naver', {}, getTokenHeader(token));
 }
 
+function loginKakao(token) {
+  return api.post('/api/users/social/kakao', {}, getTokenHeader(token))
+}
+
+function loginNormal(body) {
+  return api.post('/api/users/login', body)
+}
+
 function getUserInfo(token) {
   return api.post('/auth/users', {}, getTokenHeader(token));
 }
 
-export { loginNaver, getUserInfo };
+export { loginNaver, getUserInfo, loginKakao, loginNormal };
