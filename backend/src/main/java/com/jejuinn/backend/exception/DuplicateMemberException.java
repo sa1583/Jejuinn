53dbae75897d5f753b45beed17080b8de7627943
lee-jaewook <jaewook_lee@outlook.com>
2023-01-27 12:36:06 +0900

    :art: feat: 네이버 소셜 로그인 구현 및 디렉토리명 변경(tamnara -> jejuinn)

---
+++ backend/src/main/java/com/jejuinn/backend/exception/DuplicateMemberException.java
@@ -0,0 +1,22 @@
+package com.jejuinn.backend.exception;
+
+
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.ResponseStatus;
+
+@ResponseStatus(value = HttpStatus.CONFLICT)
+public class DuplicateMemberException extends RuntimeException {
+    public DuplicateMemberException() {
+        super();
+    }
+    public DuplicateMemberException(String message, Throwable cause) {
+        super(message, cause);
+    }
+
+    public DuplicateMemberException(String message) {
+        super(message);
+    }
+    public DuplicateMemberException(Throwable cause) {
+        super(cause);
+    }
+}
