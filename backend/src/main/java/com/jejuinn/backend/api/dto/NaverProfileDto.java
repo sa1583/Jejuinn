dc7cc93eaa5a9862f0acaa45a1e84e121c1a1c54
lee-jaewook <jaewook_lee@outlook.com>
2023-01-27 12:36:06 +0900

    :art: feat: 네이버 소셜 로그인 구현 및 디렉토리명 변경(tamnara -> jejuinn)

---
+++ backend/src/main/java/com/jejuinn/backend/api/dto/NaverProfileDto.java
@@ -0,0 +1,40 @@
+package com.jejuinn.backend.api.dto;
+
+import lombok.*;
+
+import javax.validation.constraints.NotNull;
+import java.util.Map;
+
+@Getter
+@Setter
+@Builder
+@AllArgsConstructor
+@NoArgsConstructor
+public class NaverProfileDto {
+    @NotNull
+    String nickname;
+    @NotNull
+    String name;
+    @NotNull
+    String email;
+    @NotNull
+    String gender;
+    @NotNull
+    String age;
+    @NotNull
+    String profileImage;
+    @NotNull
+    String mobile;
+
+    public static NaverProfileDto init(Map<String, String> map){
+        return NaverProfileDto.builder()
+                .nickname(map.get("nickname"))
+                .name(map.get("name"))
+                .email(map.get("email"))
+                .gender(map.get("gender"))
+                .age(map.get("age"))
+                .profileImage(map.get("profile_image"))
+                .mobile(map.get("mobile"))
+                .build();
+    }
+}
---
+++ backend/src/main/java/com/jejuinn/backend/api/dto/response/GetUserInfoPostRes.java
@@ -0,0 +1,39 @@
+package com.jejuinn.backend.api.dto.response;
+
+import com.jejuinn.backend.db.entity.User;
+import lombok.*;
+
+/**
+ * 회원정보 응답 DTO ([Post] : /auth/users)
+ */
+@Getter
+@Setter
+@Builder
+@AllArgsConstructor
+@NoArgsConstructor
+@ToString
+public class GetUserInfoPostRes {
+
+    private Long uid;
+
+    private String email;
+
+    private String username;
+
+    private String nickname;
+
+    private boolean emailReceiveAllow;
+
+    private String phone;
+
+
+    public static GetUserInfoPostRes from(User user) {
+        return GetUserInfoPostRes.builder()
+                .uid(user.getUid())
+                .email(user.getEmail())
+                .username(user.getUsername())
+                .nickname(user.getNickname())
+                .emailReceiveAllow(user.isEmailReceiveAllow())
+                .phone(user.getPhone()).build();
+    }
+}
