9acec0905a09c4e2f8518f46ee5e4f20ea6180ed
lee-jaewook <jaewook_lee@outlook.com>
2023-01-29 20:57:59 +0900

    :sparkle: refactor: 액츄에이터 추가 및 도커 설정파일 추가

---
+++ backend/src/main/java/com/jejuinn/backend/api/controller/GuestHouseController.java
@@ -0,0 +1,12 @@
+package com.jejuinn.backend.api.controller;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequiredArgsConstructor
+public class GuestHouseController {
+
+//    public ResponseEntity<?> getGuestHouseList
+}
