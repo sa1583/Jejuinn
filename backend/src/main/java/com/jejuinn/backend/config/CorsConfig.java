ea0404b3544089e8d510c9fdc29ba5c6507a9702
이재욱 <jaewook_lee@outlook.com>
2023-01-30 12:21:30 +0900

    :fire: fix: configuration

--- backend/src/main/java/com/jejuinn/backend/config/CorsConfig.java
+++ backend/src/main/java/com/jejuinn/backend/config/CorsConfig.java
@@ -2,9 +2,12 @@ package com.jejuinn.backend.config;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.http.HttpMethod;
 import org.springframework.web.cors.CorsConfiguration;
 import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
 import org.springframework.web.filter.CorsFilter;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @Configuration
 public class CorsConfig {
@@ -19,5 +22,29 @@ public class CorsConfig {
 
         source.registerCorsConfiguration("/**", config);
         return new CorsFilter(source);
+    }
+
+
+    /**
+     * CORS 설정, 허락된 URL, 허락된 Origin
+     * @return
+     */
+    @Bean
+    public WebMvcConfigurer corsConfigurer() {
+        return new WebMvcConfigurer() {
+
+            @Override
+            public void addCorsMappings(CorsRegistry registry) {
+                registry.addMapping("/**")
+                        .allowedOrigins("*")
+                        .allowedMethods(
+                                HttpMethod.GET.name(),
+                                HttpMethod.HEAD.name(),
+                                HttpMethod.POST.name(),
+                                HttpMethod.PUT.name(),
+                                HttpMethod.DELETE.name()
+                        );
+            }
+        };
     }
 }
