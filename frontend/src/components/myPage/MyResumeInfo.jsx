216d821ea5809b47926404dc179ebadf429d7303
2udy <22udy629@gmail.com>
2023-02-01 09:56:57 +0900

    <Box></Box>

---
+++ frontend/src/components/myPage/MyResumeInfo.jsx
@@ -0,0 +1,13 @@
+import { Box, Grid, styled, Button } from '@mui/material';
+export default function MyResumeInfo() {
+  return (
+    <>
+      <h1>지원서</h1>
+      <Box>이름(성별)</Box>
+      <Box>생년월일(나이)</Box>
+      <Box>휴대폰번호</Box>
+      <Box>이메일</Box>
+      <Box>SNS</Box>
+    </>
+  );
+}
