aa8e65789a02c3eb95ce6120eb4dbaaaadd808bd
이재욱 <jaewook_lee@outlook.com>
2023-02-01 09:58:09 +0900

    :hot: fix: webRtc

--- backend/src/main/java/com/jejuinn/backend/BackendApplication.java
+++ backend/src/main/java/com/jejuinn/backend/BackendApplication.java
@@ -1,15 +1,18 @@
 package com.jejuinn.backend;
 
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 import java.nio.file.Paths;
 
 @SpringBootApplication
+@Slf4j
 public class BackendApplication {
 
 	public static void main(String[] args) {
 		System.out.println(Paths.get(System.getProperty("user.home"), ".kurento","config.properties"));
+		log.info(" kurento config file path : {}", Paths.get(System.getProperty("user.home"), ".kurento","config.properties"));
 		SpringApplication.run(BackendApplication.class, args);
 	}
 
