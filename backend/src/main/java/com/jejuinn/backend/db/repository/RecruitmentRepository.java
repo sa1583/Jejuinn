b6c2a7beee8d1947fa42ca348594ac588e1d99c5
SSAFY <xvsgxv@gmail.com>
2023-02-05 23:58:12 +0900

    :sparkles: feat: 스탭 모집공고 세부정보 보기

---
+++ backend/src/main/java/com/jejuinn/backend/db/repository/RecruitmentRepository.java
@@ -0,0 +1,8 @@
+package com.jejuinn.backend.db.repository;
+
+import com.jejuinn.backend.db.entity.Recruitment;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface RecruitmentRepository extends JpaRepository<Recruitment, Long> {
+
+}
