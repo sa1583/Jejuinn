bb8a33017d7a193d90b9ca234d2306de2a2d6d7f
SSAFY <xvsgxv@gmail.com>
2023-02-05 22:00:57 +0900

    :sparkles: feat: 스탭 모집 직무 전체보기 기능 구현 + Entity 구현

--- backend/src/main/java/com/jejuinn/backend/db/entity/Work.java
+++ backend/src/main/java/com/jejuinn/backend/db/entity/Work.java
@@ -20,29 +20,38 @@ public class Work {
 
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name="uid")
     private Long uid;
 
     @ManyToOne
     @JoinColumn(name = "recruitment_uid")
     private Recruitment recruitment;
 
-    @Column(length = 25)
+    @Column(name = "work_name", length = 25)
     private String workName;
 
+    @Column(name = "intake")
     private int intake;
 
-    @Column(length = 10)
+    @Column(name = "gender", length = 10)
     private String gender;
 
+    @Column(name = "min_work_period")
     private int minWorkPeriod;
 
-    private int daysPerWeek;
-
-    @Column(length = 50)
+    @Column(name = "work_time", length = 50)
     private String workTime;
 
-    @ElementCollection
-    @CollectionTable(name = "work_days", joinColumns = @JoinColumn(name = "uid"))
-    @Column(name = "day")
-    private List<String> workDays;
+    @Column(name = "work_days")
+    private int workDays;
+
+    @Column(name = "days_off")
+    private int daysOff;
+
+    @Column(name = "work_description", length = 255)
+    private String workDescription;
+
+    @Column(name = "salary", length = 25)
+    private String salary;
+
 }
