3e45bd031c258cff18025da8520e1ac98942da4d
SSAFY <xvsgxv@gmail.com>
2023-02-07 14:45:22 +0900

    :sparkle: feat: 지원서 수정 및 삭제 기능 구현

---
+++ backend/src/main/java/com/jejuinn/backend/api/dto/request/resumeinfo/InsertResumeInfoPostReq.java
@@ -0,0 +1,67 @@
+package com.jejuinn.backend.api.dto.request.resumeinfo;
+
+import com.jejuinn.backend.db.entity.Area;
+import com.jejuinn.backend.db.entity.PersonType;
+import com.jejuinn.backend.db.entity.ResumeInfo;
+import com.jejuinn.backend.db.entity.User;
+import lombok.*;
+
+import javax.validation.constraints.NotNull;
+import java.time.LocalDate;
+import java.util.ArrayList;
+import java.util.List;
+
+@Getter
+@Setter
+@Builder
+@AllArgsConstructor
+@NoArgsConstructor
+public class InsertResumeInfoPostReq {
+
+    @NotNull
+    private Long userUid;
+
+    @NotNull
+    private String content;
+
+    @NotNull
+    private LocalDate possibleStartDate;
+
+    @NotNull
+    private int minWorkPeriod;
+
+    @NotNull
+    private boolean autoApply;
+
+    private List<String> tags;
+
+    private List<String> interestAreas;
+
+    public List<PersonType> toPersonType() {
+        List<PersonType> list = new ArrayList<>();
+        for(String s : this.getTags()) {
+            list.add(PersonType.builder().type(s).build());
+        }
+        return list;
+    }
+
+    public List<Area> toArea() {
+        List<Area> list = new ArrayList<>();
+        for(String s : this.getInterestAreas()) {
+            list.add(Area.builder().areaName(s).build());
+        }
+        return list;
+    }
+
+    public ResumeInfo toResumeInfo() {
+        return ResumeInfo.builder()
+                .user(User.builder().uid(this.userUid).build())
+                .content(this.content)
+                .possibleStartDate(this.possibleStartDate)
+                .minWorkPeriod(this.minWorkPeriod)
+                .autoApply(this.autoApply)
+                .tags(toPersonType())
+                .interestAreas(toArea())
+                .build();
+    }
+}
