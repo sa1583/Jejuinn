b0d936387107e3b9b62127a5f70d757174e1f52c
SSAFY <xvsgxv@gmail.com>
2023-02-08 10:42:15 +0900

    :sparkles: feat: 지원서 제출 기능 구현

--- backend/src/main/java/com/jejuinn/backend/api/service/ResumeInfoService.java
+++ backend/src/main/java/com/jejuinn/backend/api/service/ResumeInfoService.java
@@ -1,7 +1,11 @@
 package com.jejuinn.backend.api.service;
 
+import com.jejuinn.backend.api.dto.request.recruitment.InsertWorkResumeInfoPostReq;
 import com.jejuinn.backend.db.entity.ResumeInfo;
+import com.jejuinn.backend.db.entity.Work;
+import com.jejuinn.backend.db.entity.WorkResumeInfo;
 import com.jejuinn.backend.db.repository.ResumeInfoRepository;
+import com.jejuinn.backend.db.repository.WorkRepository;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 
@@ -13,6 +17,7 @@ import java.util.Optional;
 public class ResumeInfoService {
 
     private final ResumeInfoRepository resumeInfoRepository;
+    private final WorkRepository workRepository;
 
     @Transactional
     public ResumeInfo update(Long userUid) {
@@ -24,6 +29,18 @@ public class ResumeInfoService {
         boolean autoApply = resumeInfo.isAutoApply();
         resumeInfo.setAutoApply(!autoApply);
         return resumeInfo;
+    }
+
+    public WorkResumeInfo insertWorkResumeInfo(InsertWorkResumeInfoPostReq insertWorkResumeInfoPostReq) {
+        Optional<ResumeInfo> resumeInfo = resumeInfoRepository.findByUserUidAndIsDeletedFalse(insertWorkResumeInfoPostReq.getUserUid());
+        Optional<Work> work = workRepository.findById(insertWorkResumeInfoPostReq.getWorkUid());
+        if(resumeInfo.isPresent() && work.isPresent()) {
+            return WorkResumeInfo.builder()
+                    .resumeInfo(resumeInfo.get())
+                    .work(work.get())
+                    .build();
+        }
+        return null;
     }
 
 }
--- backend/src/main/java/com/jejuinn/backend/db/repository/ResumeInfoRepository.java
+++ backend/src/main/java/com/jejuinn/backend/db/repository/ResumeInfoRepository.java
@@ -2,6 +2,7 @@ package com.jejuinn.backend.db.repository;
 
 import com.jejuinn.backend.db.entity.ResumeInfo;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 
 import java.util.Optional;
 
