794e8d0f76036615422235ebe6ecaae3d85b39e9
DESKTOP-DOGVPUB\SSAFY <kares64@gmail.com>
2023-02-08 20:36:08 +0900

    :sparkles: feat: 마이페이지 내가 지원한 공고 UI추가

--- frontend/src/components/myPage/MyMainApply.jsx
+++ frontend/src/components/myPage/MyMainApply.jsx
@@ -1,7 +1,59 @@
-import { Box, styled, Button, IconButton } from '@mui/material';
-import ExpandMoreIcon from '@mui/icons-material/ExpandMore';
-import WhiteBox from '../whiteBox/WhiteBox';
+import { Box, Stack, Avatar, Typography } from '@mui/material';
+import { images } from '../../assets/images';
 
-export default function MyMainApply() {
-  return <Box sx={{ paddingY: '3%', paddingX: '3%' }}>모집공고~ </Box>;
+export default function MyMainApply({ id }) {
+  const data = {
+    guestHouseName: '파티 게스트하우스',
+    recruitmentImageUrl: null,
+    recruitmentTitle: '파티 게스트하우스 여자 스탭 1인 모집 ',
+    isRead: false,
+  };
+  return (
+    <Stack
+      direction="row"
+      justifyContent="space-between"
+      spacing={3}
+      sx={{ padding: '2%', display: 'flex', alignContent: 'center' }}
+    >
+      <Avatar
+        src={
+          data.recruitmentImageUrl
+            ? data.recruitmentImageUrl
+            : images.sample_profile
+        }
+        sx={{ width: '75px', height: '75px' }}
+      />
+      <Stack direction="column" justifyContent="center" width="70%">
+        <Typography>{data.guestHouseName}</Typography>
+        <Typography
+          sx={{
+            fontFamily: 'Lato',
+            fontStyle: 'normal',
+            fontWeight: 600,
+            fontSize: '24px',
+          }}
+        >
+          {data.recruitmentTitle}
+        </Typography>
+      </Stack>
+      <Box
+        component="span"
+        sx={{
+          bgcolor: data.isRead ? 'primary.main' : 'white',
+          color: data.isRead ? 'white' : 'grey',
+          border: 'solid 3px',
+          borderColor: data.isRead ? 'primary.main' : 'grey',
+          borderRadius: '50px',
+          width: 70,
+          height: 70,
+          display: 'flex',
+          justifyContent: 'center',
+          alignItems: 'center',
+          fontSize: '20px',
+        }}
+      >
+        {data.isRead ? '열람' : '미열람'}
+      </Box>
+    </Stack>
+  );
 }
