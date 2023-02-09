f3e8ff32122e13a277caa5151df3f7bd08ab4f3b
SSAFY <xvsgxv@gmail.com>
2023-02-09 10:51:19 +0900

    :sparkles: feat: 직무 지원자 리스트 제공 기능 구현

--- backend/src/main/java/com/jejuinn/backend/db/repository/ResumeInfoRepository.java
+++ backend/src/main/java/com/jejuinn/backend/db/repository/ResumeInfoRepository.java
@@ -8,5 +8,5 @@ import java.util.Optional;
 
 public interface ResumeInfoRepository extends JpaRepository<ResumeInfo, Long> {
     Optional<ResumeInfo> findByUserUidAndIsDeletedFalse(Long userUid);
-
+    Optional<ResumeInfo> findById(Long resumeInfoUid);
 }
