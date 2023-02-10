b1a1706f31c5bf50dacc4dfbd92c27bea2bfb9cc
이재욱 <jaewook_lee@outlook.com>
2023-02-10 14:33:50 +0900

    :art: feat: 추천 정보 가져오기 service 수정

---
+++ backend/src/main/java/com/jejuinn/backend/api/dto/response/recommender/RecommendResumeDto.java
@@ -0,0 +1,26 @@
+package com.jejuinn.backend.api.dto.response.recommender;
+
+import com.jejuinn.backend.db.entity.Area;
+import com.jejuinn.backend.db.entity.PersonType;
+import com.querydsl.core.annotations.QueryProjection;
+import lombok.Getter;
+import lombok.Setter;
+
+import java.util.List;
+import java.util.stream.Collectors;
+
+@Getter
+@Setter
+public class RecommendResumeDto {
+    private Long resumeInfoUid;
+    private List<String> guestHouseTypes;
+    private List<String> personTypes;
+
+    @QueryProjection
+    public RecommendResumeDto(Long resumeInfoUid, String guestHouseType) {
+        this.resumeInfoUid = resumeInfoUid;
+        if(guestHouseType != null || !guestHouseType.isEmpty()){
+            this.guestHouseTypes = List.of(guestHouseType.split("#"));
+        }
+    }
+}
